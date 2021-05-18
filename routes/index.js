e8b7e4f88a2ad0c940660f7ce6af2f29388e9147
suryanashwin4u <suryanashwin4u@gmail.com>
2021-05-18 18:03:39 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,9 @@ var userModule=require('../modules/user');
 //add bcrypt.js files for encrypting passwords
 var bcrypt=require('bcryptjs');
 
+//add jsonwebtoke files
+var jwt=require('jsonwebtoken');
+
 
 //middleware to check email already exists in the database
 function checkEmail(req,res,next){ 
@@ -40,7 +43,7 @@ function checkUsername(req,res,next){
 
 //calling login page
 router.get('/login', function(req, res, next) {  //call index.ejs and pass keywords and values mentioned here
-  res.render('login', { title_1: 'Password Management System',title_2: 'LOGIN FORM' });
+  res.render('login', { title_1: 'Password Management System',title_2: 'ENTER YOUR DETAILS INTO LOGIN FORM' });
 });
 
 //calling login page for data post using login form
@@ -52,20 +55,45 @@ router.post('/login', function(req, res, next) {  //call index.ejs and pass keyw
 	
 	//query and execution from mongo db
 	var check_user=userModule.findOne({username:username});
+
 	check_user.exec((err,data)=>{
 		if(err) throw err;
 		
-		//get pasword from returned data from database
+		//get id from returned db data
+		var getUserId=data._id;
+
+		//get pasword from returned db data
 		var getPassword=data.pswd;
+
+		//check encrypted password with the typed password
 		if(bcrypt.compareSync(pswd,getPassword))
-		{
-			res.render('login', { title_1: 'Password Management System',title_2: 'user logged in successfully'});
+		{	
+			// generate token for a user having userid after login successfull
+			var token=jwt.sign({UserId:getUserId},'loginToken');
+
+			//store token id of logged in user
+			localStorage.setItem('userToken',token);
+
+			//store username of logged in user
+			localStorage.setItem('loginUser',username);
+
+			//redirect to dashboard page after successfull login
+			res.redirect('/dashboard');
 		}
 		else
 		{
 			res.render('login', { title_1: 'Password Management System',title_2: 'invalid username and password'});
 		}
 	})});
+
+
+//calling dashboard page using get
+router.get('/dashboard', function(req, res, next) {  //call index.ejs and pass keywords and values mentioned here
+	res.render('dashboard', { title_1: 'welcome to dashboard',title_2: 'username:'});
+  });
+  
+
+
 
 //calling signup page using get method which is not secure
 router.get('/signup', function(req, res, next) {  //call index.ejs and pass keywords and values mentioned here
@@ -101,8 +129,11 @@ var userDetails=new userModule({
 userDetails.save((err,doc)=>{
 	if(err) throw err;
 	res.render('signup', { title_1: 'Password Management System',title_2: 'user registered successfully'});
+	
 })
 
+//redirect to login page after successfull signup
+res.redirect('/login');
 }
 
 });
